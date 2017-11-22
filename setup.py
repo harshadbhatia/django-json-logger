cd888c3555f68ae0200bd3f7ef6233659f2aedee
harshadbhatia <harshadbhatia2012@gmail.com>
2017-11-22 16:30:29 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ if sys.version_info < (2, 6):
     sys.exit(1)
 
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt')
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name = "django-json-logger",
@@ -17,7 +21,7 @@ setup(
     package_dir = {'': 'django_logger'},
     packages = find_packages("django_logger", exclude="tests"),
     test_suite = "tests.tests",
-    install_requires = ['setuptools'],
+    install_requires = reqs,
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
