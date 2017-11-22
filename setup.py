3d6cf69bd03225c36f5afa8041941803d232f04c
harshadbhatia <harshadbhatia2012@gmail.com>
2017-11-22 16:24:39 +1100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import sys
+if sys.version_info < (2, 6):
+    print(sys.stderr, "{}: need Python 2.6 or later.".format(sys.argv[0]))
+    print(sys.stderr, "Your Python is {}".format(sys.version))
+    sys.exit(1)
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "django-json-logger",
+    version = "0.1",
+    url = "https://github.com/harshadbhatia/django-json-logger/",
+    license = "BSD",
+    description = "A Django app for adding json log formatter",
+    author = "Harshad Bhatia",
+    author_email = "harshadbhatia2012@gmail.com",
+    package_dir = {'': 'django_logger'},
+    packages = find_packages("django_logger", exclude="tests"),
+    test_suite = "tests.tests",
+    install_requires = ['setuptools'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: System :: Logging',
+    ]
+)
