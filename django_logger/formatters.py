f5b5f4366ba20eaf8fb3a008f1e50e1328472ecd
harshadbhatia <harshadbhatia2012@gmail.com>
2017-11-29 13:53:16 +1100

    Update formatters.py

--- django_logger/formatters.py
+++ django_logger/formatters.py
@@ -6,5 +6,7 @@ class LoggerJSONFormatter(jsonlogger.JsonFormatter):
     def add_fields(self, log_record, record, message_dict):
         super(LoggerJSONFormatter, self).add_fields(log_record, record, message_dict)
         log_record = add_extra_information(record, log_record)
-        log_record['correlation_id'] = get_cid()
+        log_record['correlationId'] = get_cid()
 
+
+formatter = LoggerJSONFormatter('(correlationId) (asctime) (level) (name) (message)')
